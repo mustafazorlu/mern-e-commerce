39a87512494952f2311089fed146afb3f7be700d
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-09-09 18:08:20 +0300

    footer and policy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Policy from "./components/Policy/Policy";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -10,6 +12,8 @@ function App() {
     return (
         <>
             <Header />
+            <Policy />
+            <Footer />
         </>
     );
 }

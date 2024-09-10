5cae375e5cbb1d957259a59d92bc10865739bcc6
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-09-10 17:31:20 +0300

    dialog localstorage

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Search from "../components/Modals/Search/Search";
+import Dialog from "../components/Modals/Dialog/Dialog";
 
 const MainLayout = ({ children }) => {
     const [isSearchShow, setIsSearchShow] = useState(false);
+    const [isDialogShow, setIsDialogShow] = useState(false);
+
+    useEffect(() => {
+        const dialogStatus = localStorage.getItem("dialog")
+            ? JSON.parse(localStorage.getItem("dialog"))
+            : localStorage.setItem("dialog", JSON.stringify(true));
+        const timer = setTimeout(() => {
+            setIsDialogShow(dialogStatus);
+        }, 2000);
+
+        // clearTimeout(timer);
+    }, []);
 
     return (
         <>
@@ -12,6 +25,10 @@ const MainLayout = ({ children }) => {
             <Search
                 isSearchShow={isSearchShow}
                 setIsSearchShow={setIsSearchShow}
+            />
+            <Dialog
+                isDialogShow={isDialogShow}
+                setIsDialogShow={setIsDialogShow}
             />
             {children}
             <Footer />
